1b2e5b8e20254408f57ae734879d9c4398e24b5d
IrisWangHan <hanhan0101087@gmail.com>
2024-12-11 15:22:02 +0800

    將html移動到View下並且轉成razorpage(cshtml)移動到View下並且轉成razorpage(cshtml),建立各頁面的控制器及VIEW

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SignFlow.Models;
+
+namespace SignFlow.Controllers;
+
+public class IndexController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public IndexController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
